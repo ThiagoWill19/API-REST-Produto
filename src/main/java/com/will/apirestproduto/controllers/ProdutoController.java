e20cb0aafc1fb6bbbe904f3ee1c30e027eaf48f7
ThiagoWill19 <thiagopompeu19@gmail.com>
2022-01-21 15:10:59 -0300

    tr

--- src/main/java/com/will/apirestproduto/controllers/ProdutoController.java
+++ src/main/java/com/will/apirestproduto/controllers/ProdutoController.java
@@ -6,19 +6,18 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.will.apirestproduto.models.Produto;
 import com.will.apirestproduto.repositories.ProdutoRepository;
 
-@Controller
+@RestController
 public class ProdutoController {
 
 	@Autowired
